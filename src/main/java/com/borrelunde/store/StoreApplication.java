1751d7cf4d3bb0276eefbc9f023da6027dae42f8
Børre A. Opedal Lunde <borre97@live.no>
2025-03-01 17:34:48 +0100

    Cleaned main method

--- src/main/java/com/borrelunde/store/StoreApplication.java
+++ src/main/java/com/borrelunde/store/StoreApplication.java
@@ -2,26 +2,12 @@ package com.borrelunde.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-		var orderService = context.getBean(OrderService.class);
-		orderService.placeOrder();
-
-		var userService = context.getBean(UserService.class);
-		final User john = new User(
-				0,
-				"john.doe@mail.com",
-				"johndoe123",
-				"John Doe"
-		);
-		userService.registerUser(john);
-		userService.registerUser(john);
-
-		context.close();
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 	}
 }
