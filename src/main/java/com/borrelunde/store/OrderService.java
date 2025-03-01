6b3d37596d9245fbd0cb1830fb3326ab49a0189f
Børre A. Opedal Lunde <borre97@live.no>
2025-03-01 11:35:05 +0100

    Added dependency injection via setter

--- src/main/java/com/borrelunde/store/OrderService.java
+++ src/main/java/com/borrelunde/store/OrderService.java
@@ -8,11 +8,11 @@ public class OrderService {
 
 	private PaymentService paymentService;
 
-	public OrderService(PaymentService paymentService) {
-		this.paymentService = paymentService;
-	}
-
 	public void placeOrder() {
 		paymentService.processPayment(10.0);
+	}
+
+	public void setPaymentService(final PaymentService paymentService) {
+		this.paymentService = paymentService;
 	}
 }
