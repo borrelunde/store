72bd8fd1f87005765616614594372ba5caeb493b
Børre A. Opedal Lunde <borre97@live.no>
2025-03-02 12:04:41 +0100

    Deleted category and product entities to generate them using JPA Buddy

--- src/main/java/com/borrelunde/store/StoreApplication.java
+++ src/main/java/com/borrelunde/store/StoreApplication.java
@@ -12,27 +12,5 @@ public class StoreApplication {
 
 	public static void main(String[] args) {
 		// ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
-		var category = Category.builder()
-				.id((byte) 1)
-				.name("Furniture")
-				.build();
-
-		var productOne = Product.builder()
-				.id(1L)
-				.name("Desk")
-				.price(BigDecimal.valueOf(249.99))
-				.build();
-
-		var productTwo = Product.builder()
-				.id(2L)
-				.name("Table")
-				.price(BigDecimal.valueOf(439.99))
-				.build();
-
-		category.addProduct(productOne);
-		category.addProduct(productTwo);
-
-		System.out.printf("User: %s\n", category);
 	}
 }
