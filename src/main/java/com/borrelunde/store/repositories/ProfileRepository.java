07e45639bd41a3d2ff46cf6efc32bc891a54d7be
Børre A. Opedal Lunde <borre97@live.no>
2025-03-03 19:06:13 +0100

    Solved exercise 9.7 step 6 and 7: noticed the n+1 problem and avoided it with the @EntityGraph annotation

--- src/main/java/com/borrelunde/store/repositories/ProfileRepository.java
+++ src/main/java/com/borrelunde/store/repositories/ProfileRepository.java
@@ -1,10 +1,12 @@
 package com.borrelunde.store.repositories;
 
 import com.borrelunde.store.entities.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProfileRepository extends CrudRepository<Profile, Long> {
+	@EntityGraph(attributePaths = "user")
 	List<Profile> findProfilesByLoyaltyPointsGreaterThan(int loyaltyPoints);
 }
